76d253d46697616796dad0c2fb604e38f6805ad7
Natnael Esayas <Nanoboss44@gmail.com>
2026-05-05 19:54:22 +0300

    minor fix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,7 +9,7 @@ import { authenticateToken } from "./middleware/auth";
 const app = express();
 const PORT = process.env.PORT || 4000;
 const allowedOrigin =
-  process.env.CORS_ALLOWED_ORIGIN || "http://localhost:3000";
+  process.env.CORS_ALLOWED_ORIGIN;
 
 app.use(cors({ origin: allowedOrigin }));
 app.use(express.json());
